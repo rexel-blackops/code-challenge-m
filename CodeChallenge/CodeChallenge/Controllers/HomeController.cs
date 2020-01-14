01a0cb44dc5a5080eabb375418c7e0e0c1d6e487
Patt Rojanasthien <Patt.Rojanasthien@rexelusa.com>
2020-01-14 15:16:27 -0800

    remove API key from task2

--- CodeChallenge/CodeChallenge/Controllers/HomeController.cs
+++ CodeChallenge/CodeChallenge/Controllers/HomeController.cs
@@ -46,7 +46,7 @@ namespace CodeChallenge.Controllers
         public async Task<IActionResult> Task2()
         {
             // #TODO: Assign the API key obtain from task #1 here.
-            var apiKey = "YOLOBlackOps";
+            var apiKey = "";
 
             if (string.IsNullOrEmpty(apiKey))
             {
